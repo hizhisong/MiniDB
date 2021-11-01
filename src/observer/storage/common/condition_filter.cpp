f190c7b5c6845a4251b4128a41eb97a9ba82068b
briansun <imbriansun@gmail.com>
2021-11-01 12:28:57 +0800

    date: bug-fix on 'date' with the index

--- src/observer/storage/common/condition_filter.cpp
+++ src/observer/storage/common/condition_filter.cpp
@@ -146,7 +146,9 @@ bool DefaultConditionFilter::filter(const Record &rec) const
       // 按照C字符串风格来定
       cmp_result = strcmp(left_value, right_value);
     } break;
-    case INTS: {
+    case INTS:
+    case DATES:
+    {
       // 没有考虑大小端问题
       // 对int和float，要考虑字节对齐问题,有些平台下直接转换可能会跪
       int left = *(int *)left_value;
@@ -158,13 +160,6 @@ bool DefaultConditionFilter::filter(const Record &rec) const
       float right = *(float *)right_value;
       cmp_result = (int)(left - right);
     } break;
-    case DATES: {
-        // 没有考虑大小端问题
-        // 对int和float，要考虑字节对齐问题,有些平台下直接转换可能会跪
-        int left = *(int *)left_value;
-        int right = *(int *)right_value;
-        cmp_result = left - right;
-    }
     break;
     default: {
     }
